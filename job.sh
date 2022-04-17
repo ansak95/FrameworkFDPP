3c3b5b846bd74b8adfc4e8b2268727f705f08fc2
Anass Akrim <anass.akrim@univ-tlse3.fr>
2022-04-18 00:19:53 +0200

    Create job.sh

---
+++ job.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+pip install pandas==0.25.1
+python main.py               --folder_data            data \
+                             --E                      71.7e9 \
+                             --nu                     0.33 \
+                             --sigma_inf              78.6e6 \
+                             --K_IC                   19.7 \
+                             --nb_gauges              3 \
+                             --x_gauge                (0.003, 0.014, 0.025) \
+                             --y_gauge                (0.014, 0.014, 0.014)  \
+                             --theta_gauges           45 \
+                             --a_0_mean               0.001 \
+                             --m_mean                 3.5 \
+                             --m_std                  0.125 \
+                             --C_mean                 1e-10 \
+                             --n_train                1000  \
+                             --n_val                  100 \
+                             --n_test                 100 \
+                             --delta_k                500 \
+                             --lb_star                0.33 \
+                             --ub_star                0.95 
